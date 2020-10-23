0632aa21c54ee580d3e05ce0a2cb2802f5ac4680
author <812090869@qq.com>
2020-10-23 16:26:40 +0800

    updata

--- html/jsapi_ticket.php
+++ html/jsapi_ticket.php
@@ -1,1 +1,1 @@
-<?php exit();?>{"expire_time":1579213702,"jsapi_ticket":"kgt8ON7yVITDhtdwci0qeSBOZuywJI-DEImD39nIAP3GRsDEnSNtKLVgTIM5b9FbPITE2aiLULfao9f_4BaNAQ"}
+<?php exit();?>{"expire_time":1592810913,"jsapi_ticket":"kgt8ON7yVITDhtdwci0qeSBOZuywJI-DEImD39nIAP3wGUZ5ggcfQ2JtXfDJtgWT-eF15ZEKLxmgQngKe6CCVg"}
